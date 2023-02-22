ff42d4ab26616d32c853de7cc1a38b79863eaa74
AirDevil188 <urosculibrk@ymail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase false[200~git config --get user.namegit config --get user.namegit config --get user.emailcleargit config --global user.email urosculibrk@ymail.com>
2023-02-22 20:21:39 +0100

    started working on my rock, paper and scissors game added getComputerChoice function for picking a random string from my array

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function getComputerChoice() {
+    let array1 = ["rock", "paper", "scissors"]
+    let possibleChoice = array1[Math.floor(Math.random() * array1.length)]
+    return possibleChoice;
+         
+}
+
+function playsOne(playerSelection, computerSelection) {
+
+}
