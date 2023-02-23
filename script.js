37b6b888364b99471cf75ea4a476380c7a600b6e
AirDevil188 <urosculibrk@ymail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase false[200~git config --get user.namegit config --get user.namegit config --get user.emailcleargit config --global user.email urosculibrk@ymail.com>
2023-02-23 15:07:52 +0100

    added functions for rounds and also made changes to playOneRound function in order to get result from player and the computer in the end.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/* variables */
+let playerScore = 0;
+let computerScore = 0;
+let tiesCounter = 0;
+
+
+/* prompt */
+
+
+/* function for random choice made by the computer */
 
 function getComputerChoice() {
     let array1 = ["rock", "paper", "scissors"]
@@ -6,6 +16,65 @@ function getComputerChoice() {
          
 }
 
-function playsOne(playerSelection, computerSelection) {
+function playOneRound(playerSelection, computerSelection) {
+
+    let tie = `It's a tie! You chose ${playerSelection} and the computer chose ${computerSelection}.`;
+    let playerWin = `You win! Your ${playerSelection} beats the computer's ${computerSelection}.`;
+    let computerWin = `You lose! The computer's ${computerSelection} beats your ${playerSelection}.`;
+
+    if (playerSelection === computerSelection) {
+        console.log(tie)
+        tiesCounter++
+        return tie;
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log(playerWin)
+        playerScore++
+        return playerWin;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        console.log(computerWin)
+        computerScore++
+         return computerWin;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log(playerWin)
+        playerScore++
+        return playerWin;
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        console.log(computerWin)
+        computerScore++
+        return computerWin
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log(playerWin)
+        playerScore++
+        return playerWin;  
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log(computerWin)
+        computerScore++
+        return computerWin;
+    }
+
 
 }
+
+
+function game() {
+    
+    for ( let i = 0; i < 5; i++) {
+        const playerSelection = prompt("What's your choice rock, paper, scissors?").toLowerCase();
+        const computerSelection = getComputerChoice();
+        playOneRound(playerSelection, computerSelection);
+    
+    }
+        
+    if (playerScore > computerScore) {
+        alert(`You won! The final score is: You: ${playerScore}. Computer: ${computerScore}. Ties: ${tiesCounter}.`);
+    } else if (playerScore < computerScore) {
+        alert(`You lost! The final score is: You: ${playerScore}. Computer: ${computerScore}. Ties: ${tiesCounter}.`);
+    } else {
+        alert(`It's a tie! Final score is: You: ${playerScore}. Computer: ${computerScore}. Ties: ${tiesCounter}.`);
+    }
+}
+
+
+game();
+
